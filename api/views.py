2923986469c3dca444ed18c980b24b9bb443c14f
Yuva <yuva@codemancers.com>
2019-02-01 14:33:06 -0800

    always send feature-x as string

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ import string
 import random
 
 def index(request):
-    return JsonResponse({"string": generate_random_string()})
+    return JsonResponse({"string": "feature-x"})
 
 def generate_random_string(N = 64):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
